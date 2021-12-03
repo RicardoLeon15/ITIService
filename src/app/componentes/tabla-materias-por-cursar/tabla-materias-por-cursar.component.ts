089770856e2ddc2d6b53932c5146e958e47bf7f5
Ricardo Ignacio Gutiérrez León <59661365+RicardoLeon15@users.noreply.github.com>
2021-12-03 10:56:25 -0600

    Vistas Materias

---
+++ src/app/componentes/tabla-materias-por-cursar/tabla-materias-por-cursar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-materias-por-cursar',
+  templateUrl: './tabla-materias-por-cursar.component.html',
+  styleUrls: ['./tabla-materias-por-cursar.component.css']
+})
+export class TablaMateriasPorCursarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
